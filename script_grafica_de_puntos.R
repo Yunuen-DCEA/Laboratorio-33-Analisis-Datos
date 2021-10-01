c31f174ed053068ef8999df072aa8099657cbcc4
Yunuen-DCEA <he436161@uaeh.edu.mx>
2021-10-01 17:51:34 -0500

    INPUT - OUTPUT
    Script
    Gráficas
    R Markdown

---
+++ script_grafica_de_puntos.R
@@ -0,0 +1,74 @@
+####################
+#                  #
+# Copiar todo esto #
+#                  #
+####################
+# Hecho con gusto por Yunuén Hernández Álvarez (UAEH)
+
+# LABORATORIO - Gráficos en R con ggplot2 para Ciencia de Datos
+#Grafica de puntos en R (PARTE 1)
+
+
+
+#instalando paquete con los datos
+install.packages("gapminder")
+
+# cargando paquete con los datos
+library(gapminder)
+
+# cargando datos a entorno
+data("gapminder")
+
+# filtrando por año 2007
+gapminder2007 <- gapminder[gapminder$year == '2007', ]
+
+# echando un ojo a los datos
+head(gapminder2007)
+
+# cargar libreria ggplot2
+library(ggplot2)
+
+
+# como hacer grafica de puntos con ggplot2
+ggplot (data = gapminder2007,
+        mapping = aes (x = gdpPercap,
+                       y = lifeExp)) +
+  geom_point()
+
+# probando diferentes colores
+ggplot (data = gapminder2007,
+        mapping = aes (x = gdpPercap,
+                       y = lifeExp,
+                       color = continent)) +
+  geom_point()
+
+# probando diferentes más aestetics
+ggplot (data = gapminder2007,
+        mapping = aes (x = gdpPercap,
+                       y = lifeExp,
+                       color = continent,
+                       shape = continent,
+                       size = pop)) +
+  geom_point()
+
+# colocando texto
+ggplot (data = gapminder2007,
+        mapping = aes (x = gdpPercap,
+                       y = lifeExp,
+                       color = continent,
+                       shape = continent,
+                       size = pop)) +
+  geom_point() +
+  geom_text(label = gapminder2007$country,
+            color = "black" ,
+            size = 3)
+
+# colocando lineas de regresion
+ggplot (data = gapminder2007,
+        mapping = aes (x = gdpPercap,
+                       y = lifeExp,
+                       color = continent)) +
+  geom_point() +
+  geom_smooth(method = lm,
+              se = FALSE,
+              fullrange = TRUE)
